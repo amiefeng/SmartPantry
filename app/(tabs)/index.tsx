425b33e214123b325e710d9575bbd91107cb9c89
promise. <94502235+promiseadeliyi@users.noreply.github.com>
2025-10-02 20:39:08 +0000

    added global styles (colors) that can be used across all the app pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Colors } from './globalStyles';
 
 export default function TabOneScreen() {
   return (
